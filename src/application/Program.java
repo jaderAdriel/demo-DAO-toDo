91997e6e04d3110ee3aa0e38db61f6aabe58955c
jaderAdriel <jaderadriel7@gmail.com>
2024-12-29 12:42:54 -0300

    Add program application

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,24 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.TaskDao;
+import model.dao.UserDao;
+import model.entities.Task;
+import model.entities.User;
+
+
 public class Program {
     public static void main(String[] args) {
+        TaskDao taskDao = DaoFactory.createTaskDao();
+        UserDao userDao = DaoFactory.createUserDao();
+
+        for (User user1 : userDao.findAll()) {
+            System.out.println(user1);
+        }
+
+        for (Task task : taskDao.findAll()) {
+            System.out.println(task);
+        }
 
     }
 }
